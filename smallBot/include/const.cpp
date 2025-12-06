9e5f0ff86fd77133b5e1cb6e9edf226ebbc79d6b
lKReed <lkreed1757@gmail.com>
2025-12-06 12:30:49 -0500

    Added lemlib -LR

---
+++ smallBot/include/const.cpp
@@ -0,0 +1,26 @@
+// CONSTANTS
+
+// largest object found by vision sensor is at index 0
+int LARGEST = 0;
+
+// ids/indexes for the colors and teams
+const int RED_BLOCK_ID = 1;
+const int BLUE_BLOCK_ID = 2;
+const int TEAM_ID = RED_BLOCK_ID;
+const int OPP_ID = BLUE_BLOCK_ID;
+
+// motors for chassis and intake
+const int LEFT_FRONT = 12;
+const int LEFT_BACK = 19;
+const int RIGHT_FRONT = 13;
+const int RIGHT_BACK = 11;
+
+const int INT_CW = 2;
+const int INT_CCW = -3;
+
+const int OUT_TOP = 6;
+const int OUT_BOT = 7;
+
+
+// vision sensor port
+const int VIS_PORT = 16;
